072480ce02ea44cbaf8e4ec10f53366ba8e1a9b9
unknown <mindc1jun162@iiht.tech>
2022-08-22 17:23:58 +0530

    added new class

---
+++ Case_Study_3_/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Case_Study_3_.Models
+{
+    public class User
+    {
+        //Just to show in the git for merging purpos
+        public int UserId { get; set; }
+    }
+}
